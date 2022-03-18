ec18fd4984fce581f93bac3815045306d59eeecc
DaGeekyMongrel <dageekymongrel@pm.me>
2022-03-18 18:59:43 +0100

    backend for searching; readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# Cocktails
+
+## Installation
+
+From the project root:
+
+```
+npm i
+cd client && npm i
+```
+
+## Running the project
+
+In the project root, you can start the server by running
+
+npm start
+
+or
+
+npm run server
+
+then from the same folder, the React client can be started with
+
+npm run client
+
+To view the project, navigate to http://localhost:3000/cocktail
+
+Enjoy!
--- routes/api/cocktail.js
+++ routes/api/cocktail.js
@@ -3,10 +3,25 @@ const axios = require('axios');
 
 router.get('/', (req, res, next) => {
   console.log('Request for "/api/cocktail"');
+  const searchStr = req.query.searchStr;
+  const url = searchStr
+    ? `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchStr}`
+    : 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+
+  if (searchStr) {
+    console.log(`Searching for ${searchStr}`);
+  }
+
   axios
-    .get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .get(url)
     .then((response) => {
+      if (!response.data?.drinks || response.data.drinks.length === 0) {
+        res.status(404).send('Cocktail not found!');
+        return;
+      }
+
       const drink = response.data.drinks[0];
+
       const ingredients = Object.entries(drink)
         .filter(
           ([key, value]) =>
@@ -14,7 +29,10 @@ router.get('/', (req, res, next) => {
         )
         .sort((a, b) => a[0].substring(13) - b[0].substring(13))
         .map((entry) => entry[1]);
-      const measures = ingredients.map((val, i) => drink[`strMeasure${i + 1}`]);
+
+      const measures = ingredients.map((val, i) =>
+        drink[`strMeasure${i + 1}`] === '' ? null : drink[`strMeasure${i + 1}`]
+      );
 
       res.json({
         name: drink.strDrink,
